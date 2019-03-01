99549288f5a4dcca0ce7f46a8a06d3fcbe98b308
unknown <mirukusheki@gmail.com>
2019-03-01 22:08:50 +0700

    Updates

---
+++ precache-manifest.2263a415d28071522879078671d98175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7cd80495748112e5afb86600eb0c802",
+    "url": "/ashiyaap/static/media/yes.d7cd8049.svg"
+  },
+  {
+    "revision": "b3b650b76a3650f921495a89887d79ef",
+    "url": "/ashiyaap/static/media/ashiap.b3b650b7.mp3"
+  },
+  {
+    "revision": "94269f947340696acca6",
+    "url": "/ashiyaap/static/js/runtime~main.94269f94.js"
+  },
+  {
+    "revision": "9b7414ef4260f646259a",
+    "url": "/ashiyaap/static/js/main.9b7414ef.chunk.js"
+  },
+  {
+    "revision": "e2a032349bc2797c4db5",
+    "url": "/ashiyaap/static/js/2.e2a03234.chunk.js"
+  },
+  {
+    "revision": "9b7414ef4260f646259a",
+    "url": "/ashiyaap/static/css/main.c4884ead.chunk.css"
+  },
+  {
+    "revision": "d46d3b327dfdfeee14fd1ca691579ef7",
+    "url": "/ashiyaap/index.html"
+  }
+];
