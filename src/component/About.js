e5f7109c7b2094ab9b2295a2b18e3669dfeff333
unknown <mirukusheki@gmail.com>
2019-03-02 15:34:11 +0700

    update in about

--- src/component/About.js
+++ src/component/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="social-media">
       Created by <b>MirukuSheki</b> <br/>
       <b>Github:</b> <a href="https://github.com/mirukusheki" target="_blank">mirukusheki</a><br/>
-      <b>Paypal: </b><a href="https://paypal.me/RJKuker?locale.x=id_ID" target="_blank">RJKuker</a><br/>
+      <b>Paypal: </b><a href="https://paypal.me/RJKuker?locale.x=id_ID" target="_blank">paypal.me/RJKuker</a><br/>
       <small>(You can Donate to me for more development or just enjoying my web page)</small><br/>
       <b>Repositories:</b> <a href="https://github.com/mirukusheki/ashiyaap"
                               target="_blank">https://github.com/mirukusheki/ashiyaap</a>
