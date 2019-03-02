47868fc6b298449206fe1f38fb25129a9a100475
unknown <mirukusheki@gmail.com>
2019-03-02 08:10:11 +0700

    changing background video + CSS File

--- src/component/Video.js
+++ src/component/Video.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Video = () => {
-  let videos = 'https://www.youtube.com/embed/o3aojIlLKcM' +
+  let videos = 'https://www.youtube.com/embed/XdHS1HQn538' +
     '?autoplay=1&mute=1&controls=0&showinfo=0&disablekb=1&modestbranding=1&autohide=1';
   return (
     <div className="video-wrapper">
