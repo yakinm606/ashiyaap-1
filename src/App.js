92dd4575f6be67537a41d873169df1638e145edd
unknown <mirukusheki@gmail.com>
2019-04-02 16:07:47 +0700

    v1.0.30

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
-import logo               from "./img/yes.svg";
-import suara              from "./img/ashiap.mp3";
-import About              from './component/About';
-import Background         from './component/Background';
-import Foreground         from './component/Foreground';
-import Video              from './component/Video';
+import React, { Component } from "react";
+import logo from "./img/yes.svg";
+import suara from "./img/ashiap.mp3";
+import About from "./component/About";
+import Background from "./component/Background";
+import Foreground from "./component/Foreground";
+import Video from "./component/Video";
 import "./App.css";
 
 class App extends Component {
@@ -22,41 +22,37 @@ class App extends Component {
 
   clickedCaption = () => {
     let clicked = this.state.timesClicked;
-    let clickString = (value) => "Atta di klik sebanyak " + value + "kali."
+    let clickString = value => "Atta di klik sebanyak " + value + "kali.";
 
     // if clicked is idling set idling statement
     // if clicked just have 1 length, "sekali"
     // sisanya ya itu
-    if (clicked === 0) return(
-      <div>Klik biar Atta ngomong "Ashiap"</div>
-    )
-    else if (clicked === 1) return(
-      <div>{ clickString("se") }</div>
-    )
-    else return(
-      <div>{ clickString(`${clicked} `) }</div>
-    )
-  }
+    if (clicked === 0) return <div>Klik biar Atta ngomong "Ashiap"</div>;
+    else if (clicked === 1) return <div>{clickString("se")}</div>;
+    else return <div>{clickString(`${clicked} `)}</div>;
+  };
 
   render() {
     return (
       <div className="App" onClick={e => this.playMusic()}>
-        <Foreground/>
-        <Video/>
-        <Background/>
+        <Foreground />
+        <Video />
+        <Background />
         <div className="content">
           <header className="App-header">
-            <h1 style={{margin: '0'}}>ASHIYAAP.exe</h1>
+            <h1 style={{ margin: "0" }}>ASHIYAAP.exe</h1>
             <div>
-              <embed src={logo} className="App-logo" style={{height: '17em'}}/>
+              <embed
+                src={logo}
+                className="App-logo"
+                style={{ height: "17em" }}
+              />
             </div>
-            <div style={{marginTop: '1em'}}>
-              { this.clickedCaption() }
-            </div>
+            <div style={{ marginTop: "1em" }}>{this.clickedCaption()}</div>
           </header>
-          <About/>
+          <About />
         </div>
-        <div className="version">- Version 1.0.25</div>
+        <div className="version">- Version 1.0.30</div>
       </div>
     );
   }
